5433e10f8177f056569f52b445d8c7478d958d69
pavitrajha <65819366+pavitrajha@users.noreply.github.com>
2020-07-20 20:12:15 +0530

    Add files via upload

---
+++ hacktoolkit.sh
@@ -0,0 +1,40 @@
+echo starting all the setup for ethical hacking 
+apt install git 
+apt install python python2 
+apt install curl proot wget php openssh lolcat figlet
+pkg install root-repo 
+pkg install unstable-repo
+pkg install x11-repo
+pkg install nmap hydra netcat tshark termux-api 
+pip install lolcat figlet
+git clone https://github.com/noob-hackers/m-wiz
+git clone https://github.com/Ignitetch/AdvPhishing 
+git clone https://github.com/noob-hackers/tunnel
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/noob-hackers/grabcam
+git clone https://github.com/mohinparamasivam/Email-Bomber
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/thelinuxchoice/blackeye
+git clone https://github.com/metachar/PhoneSploit
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
+git clone https://github.com/thelinuxchoice/saycheese
+git clone https://github.com/TheSpeedX/TBomb
+git clone https://github.com/noob-hackers/Infect
+git clone https://github.com/noob-hackers/snap
+git clone https://github.com/grafov/hulk
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/noob-hackers/p-gen
+git clone https://github.com/ilkayKARATEPE/shellphish
+git clone https://github.com/T4Termux/Duck_Droid
+git clone https://github.com/Mebus/cupp 
+git clone https://github.com/rajkumardusad/Tool-X 
+git clone https://github.com/Ha3MrX/InstaBrute
+git clone https://github.com/LOoLzeC/ASU
+git clone https://github.com/technicaldada/hackerpro
+git clone https://github.com/V4N654T/fb-hacker
+mv m-wiz shellphish p-gen routersploit hulk snap Infect TBomb saycheese PhonSploit blackeye seeker Email-Bomber grabcam hacklock tunnel AdvPhishing m-wiz Duck_Droid cupp Tool-X hackerpro fb-hacker ASU InstaBrute  $HOME 
+cd 
+echo ALL THE SETUP IS DONE 
+exit
+
